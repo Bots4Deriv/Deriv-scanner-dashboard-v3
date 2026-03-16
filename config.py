6575624a748f85c597662d475cd7aaa1d79d7001
Bots4Deriv <winninggurupredictions@gmail.com>
2026-03-16 12:30:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,3 @@
-import os
+SYMBOLS = ["R_25", "R_50", "R_75", "R_100"]
 
-class Config:
-
-    DERIV_APP_ID = os.getenv("DERIV_APP_ID", "1089")
-    DERIV_WS_URL = "wss://ws.binaryws.com/websockets/v3"
-
-    SYMBOLS = os.getenv(
-        "SYMBOLS",
-        "R_25,R_50,R_75,R_100"
-    ).split(",")
-
-    UPDATE_INTERVAL = float(os.getenv("UPDATE_INTERVAL", "0.1"))
-    HISTORY_LENGTH = int(os.getenv("HISTORY_LENGTH", "100"))
+DERIV_WS = "wss://ws.derivws.com/websockets/v3?app_id=1089"
